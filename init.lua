6ae157f228265234b94844cb91224da317b5f418
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2024-05-03 16:19:42 +0300

    new theme

--- init.lua
+++ init.lua
@@ -166,17 +166,36 @@ require('lazy').setup({
 
   {
     --Theme
-    'catppuccin/nvim',
+    'rebelot/kanagawa.nvim',
     priority = 1000,
     config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        no_italic = true,    -- Force no italic
-        no_bold = true,      -- Force no bold
-        no_underline = true, -- Force no underline
-        comments = { "normal" }
+      require('kanagawa').setup({
+        compile = true,   -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = false },
+        functionStyle = {},
+        keywordStyle = { italic = false },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,   -- do not set background color
+        dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = {             -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = "wave",  -- Load "wave" theme when 'background' option is not set
+        background = {   -- map the value of 'background' option to a theme
+          dark = "wave", -- try "dragon" !
+          light = "lotus"
+        },
       })
-      vim.cmd.colorscheme 'catppuccin'
+
+      -- setup must be called before loading
+      vim.cmd("colorscheme kanagawa")
     end,
   },
 
@@ -187,7 +206,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'catppuccin',
+        theme = 'kanagawa',
         component_separators = '|',
         section_separators = '',
       },
